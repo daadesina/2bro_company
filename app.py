919df703cb12ca0c216ea76d75a413e26cf6070e
daadesina <daadesina1@gmail.com>
2024-11-20 03:55:20 +0100

    form created

--- app.py
+++ app.py
@@ -10,11 +10,25 @@ def home():
 def profile():
     return render_template('profile.html')
 
-@app.route('/part')
-def part():
-    return render_template('part.html')
+@app.route('/drug')
+def drug():
+    return render_template('drug.html')
 
+@app.route('/prescription')
+def prescription():
+    return render_template('prescription.html')
 
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/indexZ')
+def indexZ ():
+    return render_template('indexZ.html')
+
+@app.route('/create')
+def create ():
+    return render_template('create_participant.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
